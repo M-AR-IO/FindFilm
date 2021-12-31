fe2b7a277e11bb6f53bad0e905b5259ae79a3de1
kurniaalhafis123 <kurniaalhafizbangun@gmail.com>
2021-12-31 16:31:18 +0700

    Login Activity

--- app/src/main/java/my/mobile/findfilm/ui/login/LoginViewModelFactory.kt
+++ app/src/main/java/my/mobile/findfilm/ui/login/LoginViewModelFactory.kt
@@ -1,5 +1,6 @@
 package my.mobile.findfilm.ui.login
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import my.mobile.findfilm.data.LoginDataSource
@@ -9,14 +10,15 @@ import my.mobile.findfilm.data.LoginRepository
  * ViewModel provider factory to instantiate LoginViewModel.
  * Required given LoginViewModel has a non-empty constructor
  */
-class LoginViewModelFactory : ViewModelProvider.Factory {
+class LoginViewModelFactory(context: Context) : ViewModelProvider.Factory {
 
+    val dataSource = LoginDataSource(context)
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
             return LoginViewModel(
                 loginRepository = LoginRepository(
-                    dataSource = LoginDataSource()
+                    dataSource = dataSource
                 )
             ) as T
         }
